c902d5504e697f9ccaa0fa60edff59566199d0a3
Azran Hossain <mr69751@gmail.com>
2023-04-06 15:46:59 +0600

    Base DB Class & Factory Class Added
    - All of the Repo class should be extended from this base Repo class.

---
+++ DAL/DataAccessFactory.cs
@@ -0,0 +1,12 @@
+﻿using DAL.Models;
+using DAL.Repository;
+using DAL.Interface;
+using System.Collections.Generic;
+
+namespace DAL
+{
+    public static class DataAccessFactory
+    {
+        // returns all repositories from DAL
+    }
+}
