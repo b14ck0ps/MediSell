223450dd7296a7d008a4064cd3b83759e04141db
Azran Hossain <mr69751@gmail.com>
2023-04-06 15:29:38 +0600

    basic Repo Interface
    We will implement  this interface to all repository.

---
+++ DAL/Interface/IReopsitory.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Interface
+{
+    public interface IReopsitory<T, in TId, out TRet>
+    {
+        TRet Add(T entity);
+        TRet Update(T entity);
+        TRet Delete(T entity);
+        T GetById(TId id);
+        IEnumerable<T> GetAll();
+    }
+}
