7aff80650270de6571844fd7440d585509caa1d0
Azran Hossain <mr69751@gmail.com>
2023-04-06 19:34:54 +0600

    Automapper 8.1.1 Added
    - no need for any object convert methods

--- APILayer/Global.asax.cs
+++ APILayer/Global.asax.cs
@@ -1,16 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Web.Routing;
+using AutoMapper;
+using BLL.DTOs;
 
 namespace APILayer
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
+        [Obsolete("Obsolete")]
         protected void Application_Start()
         {
+            Mapper.Initialize(cfg => cfg.AddProfile<AutoMapperProfile>());
             GlobalConfiguration.Configure(WebApiConfig.Register);
         }
     }
---
+++ BLL/DTOs/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using DAL.Models;
+
+namespace BLL.DTOs
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }
+    }
+}
