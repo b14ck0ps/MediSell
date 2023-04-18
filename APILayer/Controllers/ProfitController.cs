9b83543437651fa26225b61e2eee683043c69fa5
RifatTasnia <rifattasnia521@gmail.com>
2023-04-18 20:03:59 +0600

    Profit CRUD done

--- APILayer/Controllers/ProfitController.cs
+++ APILayer/Controllers/ProfitController.cs
@@ -1,4 +1,5 @@
-﻿using BLL.Services;
+﻿using BLL.DTOs;
+using BLL.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,6 +21,80 @@ namespace APILayer.Controllers
                 return ResponseMessage(profit == null
                     ? Request.CreateResponse(HttpStatusCode.NotFound)
                     : Request.CreateResponse(HttpStatusCode.OK, profit));
+            }
+            catch (Exception e)
+            {
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.BadRequest, e.Message));
+            }
+        }
+
+
+        [HttpGet]
+        [Route("api/profits/{id}")]
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                var profit = ProfitServices.GetprofitById(id);
+                return ResponseMessage(profit == null
+                    ? Request.CreateResponse(HttpStatusCode.NotFound)
+                    : Request.CreateResponse(HttpStatusCode.OK, profit));
+            }
+            catch (Exception e)
+            {
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.BadRequest, e.Message));
+            }
+        }
+
+
+        [HttpPost]
+        [Route("api/profits")]
+        public IHttpActionResult Post([FromBody] ProfitDto profit)
+        {
+            try
+            {
+                var isAdded = ProfitServices.Addprofit(profit);
+                return ResponseMessage(isAdded
+                    ? Request.CreateResponse(HttpStatusCode.Created)
+                    : Request.CreateResponse(HttpStatusCode.BadRequest));
+            }
+            catch (Exception e)
+            {
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.BadRequest, e.Message));
+            }
+        }
+
+
+        [HttpPatch]
+        [Route("api/profits")]
+        public IHttpActionResult Patch([FromBody] ProfitDto profit)
+        {
+            try
+            {
+                var isUpdated = ProfitServices.Updateprofit(profit);
+                return ResponseMessage(isUpdated
+                    ? Request.CreateResponse(HttpStatusCode.OK)
+                    : Request.CreateResponse(HttpStatusCode.BadRequest));
+            }
+            catch (Exception e)
+            {
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+                return ResponseMessage(Request.CreateResponse(HttpStatusCode.BadRequest, e.Message));
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("api/profits/{id}")]
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                var isDeleted = ProfitServices.Deleteprofit(id);
+                return ResponseMessage(isDeleted
+                    ? Request.CreateResponse(HttpStatusCode.OK)
+                    : Request.CreateResponse(HttpStatusCode.BadRequest));
             }
             catch (Exception e)
             {
--- BLL/Services/ProfitServices.cs
+++ BLL/Services/ProfitServices.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BLL.DTOs;
 using DAL;
+using DAL.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,5 +18,38 @@ namespace BLL.Services
             var profit = Mapper.Map(ProfitRepository.GetAll(), new List<ProfitDto>());
             return profit;
         }
+
+        public static ProfitDto GetprofitById(int id)
+        {
+            var profitRepository = DataAccessFactory.GetProfitRepository();
+            var profit = Mapper.Map(profitRepository.GetById(id), new ProfitDto());
+            return profit;
+        }
+
+        public static bool Addprofit(ProfitDto profit)
+        {
+            var profitRepository = DataAccessFactory.GetProfitRepository();
+            var profitToAdd = Mapper.Map(profit, new Profit());
+            return profitRepository.Add(profitToAdd);
+        }
+
+
+        public static bool Updateprofit(ProfitDto profit)
+        {
+            var profitRepository = DataAccessFactory.GetProfitRepository();
+            var profitToUpdate = Mapper.Map(profit, new Profit());
+            return profitRepository.Update(profitToUpdate);
+        }
+
+
+        public static bool Deleteprofit(int id)
+        {
+            var profitRepository = DataAccessFactory.GetProfitRepository();
+            return profitRepository.Delete(id);
+        }
+
+
+
+
     }
 }
